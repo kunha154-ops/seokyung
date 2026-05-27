112a884bf8e44f3b738402126183f4da623b0dcf
건하강 <kunha154@gmail.com>
2026-05-27 14:29:34 +0900

    Update next config and gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['better-sqlite3'],
+  allowedDevOrigins: [
+    'initiatives-alter-satisfy-tail.trycloudflare.com',
+    'modern-walls-send.loca.lt',
+    'seven-corners-move.loca.lt'
+  ],
   experimental: {
     serverActions: {
       bodySizeLimit: '300mb',
